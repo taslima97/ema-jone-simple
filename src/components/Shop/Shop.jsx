bcb2125dc6c45de3990d3c9902fcdec048ef7d56
taslima97 <antora.taslima97@gmail.com>
2023-03-28 12:17:20 +0600

    see ui from local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -13,8 +13,18 @@ const [cart, setCart] = useState([])
    },[])
 useEffect(()=>{
     const storedCart = getShoppingCart()
+    const savedCart = []
+    for (const id in storedCart) {
+      const addedProduct =  products.find(product=>product.id===id) 
     
-},[])
+      if (addedProduct) {
+        const quantity = storedCart[id];
+      addedProduct.quantity=quantity
+      savedCart.push(addedProduct)
+      }
+      setCart(savedCart)
+    }
+},[products])
    const handelAddToCart = (product) =>{
    const newCart = [...cart, product]
    setCart(newCart)
